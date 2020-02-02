8c0f09da7b995d91253c983989bf945fae37acdc
justin leggett <justin@codingrookie.com>
2020-02-02 01:06:41 -0600

    dashboard css not importing

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Test } from "@justinal64/dashboard";
+import { TextInput } from "@justinal64/dashboard";
 
 const App = () => {
-  return <div className="App">{Test()}</div>;
+  return (
+    <div className="App">
+      <TextInput />
+    </div>
+  );
 };
 
 export default App;
