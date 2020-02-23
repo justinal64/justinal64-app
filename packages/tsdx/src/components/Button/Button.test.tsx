82193857b8b8213c55f5fb2f3efa5f38a988bab1
justin leggett <justin@codingrookie.com>
2020-02-22 22:43:56 -0600

    added react testing library to Button.test

--- packages/tsdx/src/components/Button/Button.test.tsx
+++ packages/tsdx/src/components/Button/Button.test.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { Button } from './';
 
-describe('it', () => {
+describe('Button', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('accepts title', () => {
+    const { getByText, getByRole, debug } = render(<Button />);
+    debug();
   });
 });
