6a0a0ad4e3614f69564d4137dd6307544ec28f80
justin leggett <justin@codingrookie.com>
2020-02-22 23:09:02 -0600

    added tests that need to be satisfied

--- packages/tsdx/src/components/Button/Button.test.tsx
+++ packages/tsdx/src/components/Button/Button.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import { Button } from './';
+import { maxHeaderSize } from 'http';
 
 describe('Button', () => {
   it('renders without crashing', () => {
@@ -11,7 +12,67 @@ describe('Button', () => {
   });
 
   it('accepts title', () => {
-    const { getByText, getByRole, debug } = render(<Button />);
-    debug();
+    const { container, getByText, debug } = render(<Button title="test" />);
+    debug(container);
+    expect(getByText('test')).toBeInTheDocument();
+    expect(true).toBe(false);
+  });
+
+  it('accepts classes', () => {
+    const { container, getByText, debug } = render(
+      <Button className={['test', 'tester']} />
+    );
+    expect(true).toBe(false);
+  });
+
+  it('accepts onClick event', () => {
+    const { container, getByText, debug } = render(
+      <Button
+        onClick={() => {
+          console.log('Test');
+        }}
+      />
+    );
+    expect(true).toBe(false);
+  });
+
+  it('accepts disabled state', () => {
+    const { container, getByText, debug } = render(<Button disabled={true} />);
+    expect(true).toBe(false);
+  });
+
+  it('accepts custom attributes', () => {
+    const { container, getByText, debug } = render(
+      <Button
+        attr={[
+          { 'data-testid': 'custom-element' },
+          { 'data-testid': 'custom-element1' },
+        ]}
+      />
+    );
+    expect(true).toBe(false);
+  });
+
+  it('accepts icon', () => {
+    const { container, getByText, debug } = render(
+      <Button icon="linktoimage.svg" />
+    );
+    expect(true).toBe(false);
+  });
+
+  it('accepts size', () => {
+    const { container, getByText, debug } = render(
+      <Button size="Enum Size.Small" />
+    );
+    expect(true).toBe(false);
+  });
+
+  it('accepts children', () => {
+    const { container, getByText, debug } = render(
+      <Button>
+        <div>Hi I'm a child being passed down</div>
+      </Button>
+    );
+    expect(true).toBe(false);
   });
 });
