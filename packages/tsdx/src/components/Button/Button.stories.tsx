db4aefe99b99340dd8eb4e4fe2f942b020d410f5
justin leggett <justin@codingrookie.com>
2020-05-21 21:54:14 -0500

    added stories for button

--- packages/tsdx/src/components/Button/Button.stories.tsx
+++ packages/tsdx/src/components/Button/Button.stories.tsx
@@ -5,8 +5,27 @@ export default {
   title: 'Button',
 };
 
-export const toStorybook = () => <Button />;
+const buttonNoProps = () => <Button />;
 
-toStorybook.story = {
+buttonNoProps.story = {
   name: 'No Props',
 };
+
+const buttonWithProps = () => <Button title="Test" />;
+
+buttonWithProps.story = {
+  name: 'With Props',
+};
+
+const buttonWithFunction = () => (
+  <Button
+    title="With onClick"
+    onClick={() => console.log('Did this work!!!')}
+  />
+);
+
+buttonWithFunction.story = {
+  name: 'With Function',
+};
+
+export { buttonNoProps, buttonWithProps, buttonWithFunction };
