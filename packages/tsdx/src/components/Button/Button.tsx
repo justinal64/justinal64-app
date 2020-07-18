00585f0c6cc32e4c92718213ca02731acdcba964
justin leggett <justin@codingrookie.com>
2020-07-17 23:59:50 -0500

    added example using scss

--- packages/tsdx/src/components/Button/Button.tsx
+++ packages/tsdx/src/components/Button/Button.tsx
@@ -7,7 +7,11 @@ interface Props {
 }
 
 export const Button = (props: Props) => {
-  const { title, className, onClick = () => console.log('Test') } = props;
+  const {
+    title,
+    className = 'test',
+    onClick = () => console.log('Test'),
+  } = props;
 
   return (
     <button data-testid="button" className={className} onClick={onClick}>
---
+++ packages/ui/stories/Thing.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Thing, Props } from '../src';
+import '../src/test.scss';
+
+export default {
+  title: 'Welcome',
+};
+
+// By passing optional props to this story, you can control the props of the component when
+// you consume the story in a test.
+export const Default = (props?: Partial<Props>) => <Thing {...props} />;
